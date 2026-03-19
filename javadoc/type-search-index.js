d0c4f9aafcde2b316c2d881665e9583639c77ee8
tquadrat <thomas.thrien@tquadrat.org>
2026-03-19 21:54:28 +0100

    Some minor changes (2026-03-19T21:52:01.001262321+01:00[Europe/Berlin])

--- javadoc/type-search-index.js
+++ javadoc/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.tquadrat.foundation.email","l":"EmailAddressStringConverter"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html","k":"18"},{"p":"org.tquadrat.foundation.email","l":"EmailAddressStringConverter"}];updateSearchResults();
